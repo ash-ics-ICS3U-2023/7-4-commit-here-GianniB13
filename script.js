76230e0427286d662d20dd6e9fd48c815e2bffbe
GianniB13 <159183504+GianniB13@users.noreply.github.com>
2024-06-06 13:46:18 -0400

    htmljsnd

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 /**
  * This file will get loaded by index.html
  */
+
+'use strict';
+
+let clicks = 0;
+
+document.getElementById("my_button").addEventListener("click", my_func)
+
+function my_func() {
+  console.log("I was clicked!")
+}
